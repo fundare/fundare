aa9e158dde51b85555a3fb82c2d087436af1fef0
bnonni <bryanwnonni@gmail.com>
2019-07-06 13:56:08 -0400

    init base files for react-native

--- app/client/src/components/auth/Login.js
+++ app/client/src/components/auth/Login.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
@@ -35,7 +36,9 @@ class Login extends Component {
     }
 
     onChange = e => {
-        this.setState({ [e.target.id]: e.target.value });
+        this.setState({
+            [e.target.id]: e.target.value
+        });
     };
 
     onSubmit = e => {
@@ -52,75 +55,71 @@ class Login extends Component {
     render() {
         const { errors } = this.state;
 
-        return (
-            <div className="container">
-                <div style={{ marginTop: "4rem" }} className="row">
-                    <div className="col s8 offset-s2">
-                        <Link to="/" className="btn-flat waves-effect">
-                            <i className="material-icons left">keyboard_backspace</i> Back to
-                            home
-                        </Link>
-                        <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-                            <h4>
-                                <b>Login</b> below
-                            </h4>
-                            <p className="grey-text text-darken-1">
-                                Don't have an account? <Link to="/register">Register</Link>
-                            </p>
-                        </div>
-                        <form noValidate onSubmit={this.onSubmit}>
-                            <div className="input-field col s12">
-                                <input
-                                    onChange={this.onChange}
-                                    value={this.state.email}
-                                    error={errors.email}
-                                    id="email"
-                                    type="email"
-                                    className={classnames("", {
-                                        invalid: errors.email || errors.emailnotfound
-                                    })}
-                                />
-                                <label htmlFor="email">Email</label>
-                                <span className="red-text">
-                  {errors.email}
-                                    {errors.emailnotfound}
-                </span>
-                            </div>
-                            <div className="input-field col s12">
-                                <input
-                                    onChange={this.onChange}
-                                    value={this.state.password}
-                                    error={errors.password}
-                                    id="password"
-                                    type="password"
-                                    className={classnames("", {
-                                        invalid: errors.password || errors.passwordincorrect
-                                    })}
-                                />
-                                <label htmlFor="password">Password</label>
-                                <span className="red-text">
-                  {errors.password}
-                                    {errors.passwordincorrect}
-                </span>
-                            </div>
-                            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-                                <button
-                                    style={{
-                                        width: "150px",
-                                        borderRadius: "3px",
-                                        letterSpacing: "1.5px",
-                                        marginTop: "1rem"
-                                    }}
-                                    type="submit"
-                                    className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                                >
-                                    Login
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+        return ( <div className = "container">
+            <div style = { { marginTop: "4rem" } }
+            className = "row" >
+            <div className = "col s8 offset-s2" >
+            <Link to = "/" className = "btn-flat waves-effect" >
+            <i className = "material-icons left" > keyboard_backspace </i> Back to home </Link> <div className = "col s12" style = { { paddingLeft: "11.250px" } }>
+            <h4>
+            <b> Login < /b> below < /h4 > <p className = "grey-text text-darken-1" > Don 't have an account? <Link to="register">Register</Link> </p > </div> <form noValidate onSubmit = { this.onSubmit } >
+            <
+            div className = "input-field col s12" >
+            <
+            input onChange = { this.onChange }
+            value = { this.state.email }
+            error = { errors.email }
+            id = "email"
+            type = "email"
+            className = {
+                classnames("", {
+                    invalid: errors.email || errors.emailnotfound
+                })
+            }
+            /> <
+            label htmlFor = "email" > Email < /label> <
+            span className = "red-text" > { errors.email } { errors.emailnotfound } <
+            /span> < /
+            div > <
+            div className = "input-field col s12" >
+            <
+            input onChange = { this.onChange }
+            value = { this.state.password }
+            error = { errors.password }
+            id = "password"
+            type = "password"
+            className = {
+                classnames("", {
+                    invalid: errors.password || errors.passwordincorrect
+                })
+            }
+            /> <
+            label htmlFor = "password" > Password < /label> <
+            span className = "red-text" > { errors.password } { errors.passwordincorrect } <
+            /span> < /
+            div > <
+            div className = "col s12"
+            style = {
+                { paddingLeft: "11.250px" }
+            } >
+            <
+            button style = {
+                {
+                    width: "150px",
+                    borderRadius: "3px",
+                    letterSpacing: "1.5px",
+                    marginTop: "1rem"
+                }
+            }
+            type = "submit"
+            className = "btn btn-large waves-effect waves-light hoverable blue accent-3" >
+            Login <
+            /button> < /
+            div > <
+            /form> < /
+            div > <
+            /div> < /
+            div >
         );
     }
 }
@@ -134,6 +133,5 @@ const mapStateToProps = state => ({
     errors: state.errors
 });
 export default connect(
-    mapStateToProps,
-    { loginUser }
+    mapStateToProps, { loginUser }
 )(Login);
--- app/config/keys.js
+++ app/config/keys.js
@@ -3,6 +3,6 @@
 var mongo_password = process.env.mongo_password;
 
 module.exports = {
-    mongoURI: "mongodb+srv://bryan:" + mongo_password + "$$@cluster0-ockae.gcp.mongodb.net/test?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://bryan:" + mongo_password + "$$@cluster0-ockae.gcp.mongodb.net/fundare?retryWrites=true&w=majority",
     secretOrKey: "secret"
 };
