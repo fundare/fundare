989b41fdae4394344837815aba0cffd50377cf85
bnonni <bryanwnonni@gmail.com>
2019-07-08 15:35:43 -0400

    clearing branch for app code

--- app/config/keys.js
+++
@@ -1,8 +0,0 @@
-/*jshint esversion: 6 */
-
-var mongo_password = process.env.mongo_password;
-
-module.exports = {
-    mongoURI: "mongodb+srv://bryan:" + mongo_password + "$$@cluster0-ockae.gcp.mongodb.net/fundare?retryWrites=true&w=majority",
-    secretOrKey: "secret"
-};
