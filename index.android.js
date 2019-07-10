282e1700ec51331ea53165b8786c47c7b176b11a
bnonni <bryanwnonni@gmail.com>
2019-07-10 00:48:57 -0400

    working login screen

---
+++ index.android.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import {
+ AppRegistry,
+ StyleSheet,
+ Text,
+ View,
+ Image,
+ TextInput,
+ TouchableOpacity
+} from 'react-native';
+
+const background = require("./background.jpg");
+const lockIcon = require("./lock.png");
+const personIcon = require("./person.png");
+
+export default class FundareReactNative extends Component {
+ render() {
+  return (
+   <Image
+    style={[styles.background, styles.container]}
+    source={background}
+    resizeMode="cover"
+   >
+    <View style={styles.container} />
+    <View style={styles.wrapper}>
+     <View style={styles.inputWrap}>
+      <View style={styles.iconWrap}>
+       <Image
+        source={personIcon}
+        style={styles.icon}
+        resizeMode="contain"
+       />
+      </View>
+      <TextInput
+       placeholder="Username"
+       style={styles.input}
+       underlineColorAndroid="transparent"
+      />
+     </View>
+     <View style={styles.inputWrap}>
+      <View style={styles.iconWrap}>
+       <Image
+        source={lockIcon}
+        style={styles.icon}
+        resizeMode="contain"
+       />
+      </View>
+      <TextInput
+       placeholder="Password"
+       secureTextEntry
+       style={styles.input}
+       underlineColorAndroid="transparent"
+      />
+     </View>
+     <TouchableOpacity activeOpacity={.5}>
+      <View style={styles.button}>
+       <Text style={styles.buttonText}>Sign In</Text>
+      </View>
+     </TouchableOpacity>
+     <TouchableOpacity activeOpacity={.5}>
+      <View>
+       <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
+      </View>
+     </TouchableOpacity>
+    </View>
+    <View style={styles.container} />
+   </Image>
+  );
+ }
+}
+
+const styles = StyleSheet.create({
+ container: {
+  flex: 1,
+ },
+ background: {
+  width: null,
+  height: null
+ },
+ wrapper: {
+  paddingHorizontal: 15,
+ },
+ inputWrap: {
+  flexDirection: "row",
+  marginVertical: 10,
+  height: 40,
+  backgroundColor: "transparent"
+ },
+ input: {
+  flex: 1,
+  paddingHorizontal: 10,
+  backgroundColor: '#FFF'
+ },
+ iconWrap: {
+  paddingHorizontal: 7,
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: "#d73352"
+ },
+ icon: {
+  width: 20,
+  height: 20,
+ },
+ button: {
+  backgroundColor: "#d73352",
+  paddingVertical: 15,
+  marginVertical: 15,
+  alignItems: "center",
+  justifyContent: "center"
+ },
+ buttonText: {
+  color: "#FFF",
+  fontSize: 18
+ },
+ forgotPasswordText: {
+  color: "#FFF",
+  backgroundColor: "transparent",
+  textAlign: "center"
+ }
+});
+
+AppRegistry.registerComponent('FundareReactNative', () => FundareReactNative);
