04f634a8927d5e43f498d5cd9a3dd2a287368e39
hju <hju5@student.gsu.edu>
2019-07-19 09:19:29 -0400

    changed a line in google_map_services.dart

--- flutter_googlemap/lib/google_map_services.dart
+++ flutter_googlemap/lib/google_map_services.dart
@@ -6,7 +6,7 @@ const apiKey = "";
 class GoogleMapsServices{
   Future<String> getRouteCoordinates(LatLng l1, LatLng l2)async{
 //    String url = "https://maps.googleapis.com/maps/api/directions/json?origin=33.8,-84.2&destination=33.74,-84.33&key=$apiKey";
-    String url = "https://maps.googleapis.com/maps/api/directions/json?origin=${l1.latitude},${l1.longitude}&destination=${l2.latitude},${l2.longitude}&key=$apiKey";
+    String url = "https://maps.googleapis.com/maps/api/directions/json?origin=${l1.latitude},${l1.longitude}&destination=${l2.latitude},${l2.longitude}&key=$apiKey&mode=walking";
     http.Response response = await http.get(url);
     Map values = jsonDecode(response.body);
     print('The value of the input is: '+values["status"]);
